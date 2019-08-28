da54814854fa39455bc8dc53e75ba72b8292ad61
Sangeeth Jagadish (UST <U24186@ust-global.com>
2019-08-28 18:36:44 +0530

    Added a few bad code

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,8 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
+
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main(String args[])
@@ -11,4 +13,31 @@ public class Application {
 
       System.out.println("Is this a violation");
     }
+
+    public static void horribleIteration(String [] words){
+        int i = 0;
+        try {
+            while(true){
+                System.out.println(words[i]);
+                i++;
+            }
+        } catch (IndexOutOfBoundsException e){
+            //iteration complete
+        }
+    }
+
+    public static void readWritePrivate() throws NoSuchFieldException, IllegalAccessException {
+        Field f = System.class.getDeclaredField("lineSeparator");
+        f.setAccessible(true);
+        String separator = (String) f.get(System.class);
+        System.out.println("Line separator is " + separator + ".");
+
+        f.set(System.class ,"!!!");
+        System.out.println("Line one");
+        System.out.println("Line two");
+        System.out.println("Line three");
+    }
+
+
+
 }
