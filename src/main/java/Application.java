ca67990308b63b53fe84a06dd2b6172530dac8b9
Sangeeth Jagadish (UST <U24186@ust-global.com>
2019-08-28 18:17:03 +0530

    checking syso for violations

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,11 +2,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Application {
-    privare static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main(String args[])
     {
       logger.debug("Started the logging");
       logger.debug("This is a unique transaction" + "SSN is 000-00-0000");
       logger.debug("This transaction is done by name"+ "Sangeeth Jagadish");
+
+      System.out.println("Is this a violation");
     }
 }
