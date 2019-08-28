fa300168c4146ffd7cd121c0d046748f207f8acf
Sangeeth Jagadish (UST <U24186@ust-global.com>
2019-08-28 15:28:54 +0530

    fixed issues around identifiers

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Application {
-    privare static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main(String args[])
     {
       logger.debug("Started the logging");
